7ffb5993936d9d9a60fa96b649367053235ef75e
ac2393921 <ac2393921@gmail.com>
2025-03-18 22:12:08 +0900

    feat: book_serviceの追加

---
+++ book_service/src/main.rs
@@ -0,0 +1,24 @@
+use actix_web::middleware::{NormalizePath, TrailingSlash};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    initialize_server().await
+}
+
+async fn initialize_server() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .wrap(NormalizePath::new(TrailingSlash::Trim))
+            .route("/book", web::get().to(book))
+        // .app_data(web::Data::new(order_service.clone()))
+        // .service(api::place_order)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+async fn book() -> impl Responder {
+    HttpResponse::Ok().body("book service")
+}
